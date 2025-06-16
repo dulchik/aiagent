3371895fbb4481367da354ec13e80072dcfb68ce
dulchik <bubnyukab@gmail.com>
2025-06-16 19:12:58 +0200

    added running python files functionality

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,23 +1,21 @@
 import os
-
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_file = os.path.join(working_directory, file_path)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not os.path.abspath(file_path).startswith(abs_working_dir):
+    if not abs_file_path.startswith(abs_working_dir):
         f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_file):
+    if not os.path.isfile(abs_file_path):
         f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        MAX_CHARS = 10000
-
-        with open(target_file, "r") as f:
+        with open(abs_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
         
-        if len(file_content_string) == MAX_CHARS:
-            file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+            if len(file_content_string) == MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
         
         return file_content_string
     except Exception as e:
-        return f"Error: {e}"
+        return f'Error reading file "{file_path}": {e}'
